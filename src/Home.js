f2b1a246d63f506420b086af23acde29adef16e8
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-30 21:29:39 +0200

    Add click event to component Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const handleCLick = () => {
+    console.log("Cześć Czarek!");
+  };
   return (
     <div className="home">
       <h2>Strona główna</h2>
+      <button onClick={handleCLick}>Sprawdź</button>
     </div>
   );
 };
