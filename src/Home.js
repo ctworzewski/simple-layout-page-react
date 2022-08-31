2699ab0bec7b99e1745822336434807b41f1b102
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-31 07:40:21 +0200

    Using State hook, change value name and age

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = 'czarek';
+
+  const [name, setName]  = useState('czarek');
+  const [age, setAge] = useState(29);
   const handleCLick = () => {
-    console.log("Cześć Czarek!");
+    setName('emil');
+    setAge(30)
   };
   return (
     <div className="home">
       <h2>Strona główna</h2>
+      <p>{name} ma {age} lat.</p>
       <button onClick={handleCLick}>Sprawdź</button>
     </div>
   );
